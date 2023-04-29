2869b565e774436fe7fa3d04646797c0940bb2dc
molvin <akerstrom.perry@gmail.com>
2023-04-29 16:36:15 +0200

    truck temp

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    /*TODO: 
+    *      Load Level from saved file
+    *      Wait for player to finish level, or die
+    *          Finish Level
+    *               Go to Map screen (?) select new level or whatever
+    *          Die
+    *               UI for retry or back to map screen
+    *          
+    */
+    
+}
