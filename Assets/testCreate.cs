58d1dd3d10d8d6aa38e8609437b411408247ffea
Niklas Landberg <niklas-landberg@hotmail.com>
2023-04-29 17:13:04 +0200

    added marchingcube

---
+++ Assets/testCreate.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class testCreate : MonoBehaviour
+{
+    public PointCloudManager PCM;
+    public float scale;
+    public float maxIsoDepth;
+    // Start is called before the first frame update
+    // Update is called once per frame
+    void Update()
+    {
+        createNoiseEnvo();
+    }
+
+    public void createNoiseEnvo()
+    {
+        PCM.InitializeIsoSurfaceSphere(Vector3.zero, 10f, SuperNoise);
+      
+    }
+    public float SuperNoise(Vector3 point)
+    {
+        return 0f;
+        Perlin3D.scale = scale;
+        return Perlin3D.PerlinNoise3D(point) * maxIsoDepth;
+    }
+}
