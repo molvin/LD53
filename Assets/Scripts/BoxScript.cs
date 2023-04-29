0b82d18f8225cdd8fb8c8ceed5004ff38a666e06
daniel-fors <daniel.lennart.fors@gmail.com>
2023-04-29 23:45:22 +0200

    box :)

--- Assets/Scripts/BoxScript.cs
+++ Assets/Scripts/BoxScript.cs
@@ -2,24 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class BoxScript : MonoBehaviour
 {
     public HoverController Owner;
-    public Vector3 Offset = new Vector3(0.0f, 0.5f, -1.0f);
-    public float GravitySuction = 100.0f;
-    public float SuctionDistance = 3.0f;
+    public Vector3 Offset = new Vector3(0.0f, 0.35f, -1.0f);
 
+    private Vector3 PreviousPosition;
+    private Vector3 PreviousVelocity;
     private new Rigidbody rigidbody;
-    private MeshFilter Mesh;
+    private Vector3 velocity;
 
     private void Awake()
     {
         rigidbody = GetComponent<Rigidbody>();
-        Mesh = GetComponent<MeshFilter>();
+        PreviousPosition = transform.position;
     }
 
     private void FixedUpdate()
     {
+        if (!rigidbody.isKinematic)
+        {
+            return;
+        }
+        transform.rotation = Owner.transform.rotation;
+        Vector3 deltaPosition = Owner.transform.position + Owner.transform.rotation * Offset - transform.position;
+        transform.position += deltaPosition;
+
+        Vector3 newVelocity = (transform.position - PreviousPosition) / Time.fixedDeltaTime;
+        if ((newVelocity - PreviousVelocity).magnitude > 10.0f)
+        {
+            rigidbody.isKinematic = false;
+            rigidbody.velocity = newVelocity + Vector3.up * 3.0f;
+        }
+        PreviousVelocity = newVelocity;
+        PreviousPosition = transform.position;
     }
 }
