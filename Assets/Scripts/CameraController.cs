73a5c0826f8947a889e169e51a5d77ccdb1996a7
molvin <akerstrom.perry@gmail.com>
2023-04-29 21:07:57 +0200

    stuff

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,16 +4,21 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Vector3 Offset;
-    public float Smoothing;
     public Transform Target;
+    public Vector3 TargetOffset;
+    public Vector3 LookOffset;
+    public float Smoothing;
     private Vector3 velocity;
+
 
     private void FixedUpdate()
     {
-        Vector3 targetPos = Target.transform.position + Target.localRotation * Offset;
+
+        Vector3 targetPos = Target.transform.position + Target.localRotation * TargetOffset;
         transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, Smoothing * Time.fixedDeltaTime);
-        transform.LookAt(Target, Vector3.up);
+
+        Vector3 lookPos = Target.transform.position + Target.localRotation * LookOffset;
+        transform.LookAt(lookPos, Vector3.up);
     }
 
 }
