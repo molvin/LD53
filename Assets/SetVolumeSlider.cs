3803f5d01f89a0d3feff1ef5a4098723303c8954
Niklas Landberg <niklas-landberg@hotmail.com>
2023-04-30 23:39:10 +0200

    volume settings

---
+++ Assets/SetVolumeSlider.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SetVolumeSlider : MonoBehaviour
+{
+
+    public AudioMixer mixer;
+    public string floatName = "Master";
+    public Slider slider;
+
+
+
+    public void Start()
+    {
+        slider.SetValueWithoutNotify(1f);
+    }
+    public void SetLevel(float slider_value)
+    {
+        mixer.SetFloat(floatName, Mathf.Log10(slider_value) * 20);
+    }
+}
