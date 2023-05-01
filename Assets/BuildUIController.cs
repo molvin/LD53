f18eabba54f77e98ac6521bd8dc9900627ffb8be
Daniel Åkesson <argilstudio@gmail.com>
2023-05-01 14:07:13 +0200

    i dont know

--- Assets/BuildUIController.cs
+++ Assets/BuildUIController.cs
@@ -1,81 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using static SplineNoise3D;
 
 public class BuildUIController : MonoBehaviour
 {
-    public GameObject DoodadPrefab;
-    public GameObject ShadowObject;
+    public GameObject DoodadView;
+    public GameObject GateView;
+    public LevelMakerDoodadPlacer doodadPlacer;
+    public LevelMakerEditorController gatePlacer;
+    public List<GameObject> doodads;
+    public int UiLayer;
+
+    public void Start()
+    {
+        Button[] doodads = DoodadView.GetComponentsInChildren<Button>(true);
+        for(int i=0;i< doodads.Length; i++)
+        {
+            int x = i;
+            doodads[i].onClick.AddListener(() => setPlacer(x));
+        }
+
+        Button[] gates = GateView.GetComponentsInChildren<Button>(true);
+        for (int i = 0; i < doodads.Length; i++)
+        {
+            int x = i;
+            doodads[i].onClick.AddListener(() => setPlacer(x));
+        }
+    }
 
     public void Update()
     {
-        Spline s = GetPointOnSpline();
-        Vector3 p1 = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-        Debug.DrawLine(p1, s.pos, Color.black);
+        NumPress();
     }
-
-    public Spline GetPointOnSpline()
+    public void PressDoodad()
     {
-        Vector3 p1 = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0));
-        Vector3 p2 = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 1));
-        Vector3 delta = p2 - p1;
-        
-        float closest = float.MaxValue;
-        Spline splie = new Spline { };
-        for (int i=0; i < SplineLine.Count; i++)
-        {
-            Vector3 delta2 = SplineLine[i].pos - p1;
-            //float distance = Vector3.Dot(delta.normalized, delta2);
-            float distance = delta2.magnitude;
-            Vector3 testPoint = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, distance));
-            Debug.DrawLine(p1, testPoint, Color.magenta);
-            Spline s = getLerpSplineFromPoint(testPoint);
-            if (closest > (testPoint - s.pos).magnitude)
-            {
-                closest = (testPoint - s.pos).magnitude;
-                splie = s;
-            }
-        }
-        return splie;
+        gatePlacer.DisableEdit = true;
+        doodadPlacer.enabled = true;
+        DoodadView.SetActive(true);
+        GateView.SetActive(false);
     }
-
-    public Vector3 getClosestPointBetweenTwoLines(Vector3 p1, Vector3 p2, Vector3 p3, Vector3 p4)
+    public void PressGate()
     {
-        Vector3 P1 = p1;
-        Vector3 P2 = p3;
-        Vector3 V1 = p2 - p1;
-        Vector3 V2 = p4 - p3;
-        Vector3 V21 = P2 - P1;
-
-        float v22 = Vector3.Dot(V2, V2);
-        float v11 = Vector3.Dot(V1, V1);
-        float v21 = Vector3.Dot(V2, V1);
-        float v21_1 = Vector3.Dot(V21, V1);
-        float v21_2 = Vector3.Dot(V21, V2);
-        float denom = v21 * v21 - v22 * v11;
-
-        //s and t
-        float s;
-        float t;
-        if (Mathf.Approximately(denom, 0f))
+        gatePlacer.DisableEdit = false;
+        doodadPlacer.enabled = false;
+        DoodadView.SetActive(false);
+        GateView.SetActive(true);
+    }
+    private void setPlacer(int value)
+    {
+        Debug.Log("Set placer" + value);
+        if (!gatePlacer.DisableEdit)
         {
-            s = 0f;
-            t = (v11 * s - v21_1) / v21;
+            gatePlacer.SetCurrentSelection(value);
         }
         else
         {
-            s = (v21_2 * v21 - v22 * v21_1) / denom;
-            t = (-v21_1 * v21 + v11 * v21_2) / denom;
+            if (value >= doodads.Count) return;
+            doodadPlacer.SetDoodad(doodads[value]);
+        }      
+    }
+    private void NumPress()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            setPlacer(0);
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            setPlacer(1);
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            setPlacer(2);
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            setPlacer(3);
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            setPlacer(4);
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+            setPlacer(5);
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+            setPlacer(6);
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+            setPlacer(7);
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+            setPlacer(8);
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+            setPlacer(9);
+    }
+
+    //Returns 'true' if we touched or hovering on Unity UI element.
+    private bool IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+    {
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.layer == UiLayer)
+                return true;
         }
-
-        s = Mathf.Max(Mathf.Min(s, 1f), 0f);
-        t = Mathf.Max(Mathf.Min(t, 1f), 0f);
-
-        Vector3 p_a = P1 + s * V1;
-        Vector3 p_b = P2 + t * V2;
-        Debug.DrawLine(p_a, p_b);
-        return p_a;
+        return false;
+    }
+    //Gets all event system raycast results of current mouse or touch position.
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
     }
 }
